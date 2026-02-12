e37e709d4cbb23dfa261ef33a07e5f53d5de0422
gxfi1e <157596143+gxfi1e@users.noreply.github.com>
2026-02-12 14:01:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ window.SIGNUP_CONFIG = {
   endpoint: "https://script.google.com/macros/s/AKfycbzSNVgtOrjKp95rRlqOjb3v_IaKpCE4UREg0lK_ytMFe7FNdL7vIEYCdS77cXQXRFDe/exec",
   timezoneLabel: "Asia/Riyadh",
 
-  gameTimeText: "Friday 6:30 PM",
+  gameTimeText: "Friday 7-9.30PM",
   locationText: "KAUST Stadium",
-  googleMapsLink: "https://maps.google.com/?q=KAUST+Stadium",
+  googleMapsLink: "https://maps.app.goo.gl/FRdURVCfe1TDdVug7",
 
   teamsMinForFour: 20
 };
