b1fabe74cc66e2c083d3400098354b8c9466931b
gxfi1e <157596143+gxfi1e@users.noreply.github.com>
2026-04-02 17:32:06 +0300

    Improved game modify strategy

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.SIGNUP_CONFIG = {
   endpoint: "https://script.google.com/macros/s/AKfycbzSNVgtOrjKp95rRlqOjb3v_IaKpCE4UREg0lK_ytMFe7FNdL7vIEYCdS77cXQXRFDe/exec",
   timezoneLabel: "Asia/Riyadh",
 
-  gameTimeText: "Friday 7-9.30PM",
+  gameTimeText: "Friday 7-10PM",
   locationText: "KAUST Stadium",
   googleMapsLink: "https://maps.app.goo.gl/FRdURVCfe1TDdVug7",
 
